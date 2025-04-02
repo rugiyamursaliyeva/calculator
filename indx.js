f3093efda67dd736310c9fc5b51d23056daa4acc
rugiyamursaliyeva <ruqiyemurseliyeva@gmail.com>
2025-04-02 16:51:58 +0400

    finish

--- indx.js
+++ indx.js
@@ -1,26 +1,39 @@
-const display = document.getElementById("display")
-const numbers = document.querySelectorAll(".numbers")
-const operators = document.querySelectorAll(".operators")
+const display = document.getElementById("display");
+const numbers = document.querySelectorAll(".numbers");
+const operators = document.querySelectorAll(".operators");
 
-const clear = document.querySelector(".clear")
-const equal = document.querySelector(".equal")
+const clear = document.querySelector(".clear");
+const equal = document.querySelector(".equal");
 
 numbers.forEach(number => {
     number.addEventListener("click", function () {
-        display.value += number.textContent
-    })
-})
+        display.value += number.textContent;
+    });
+});
 
 operators.forEach(operator => {
     operator.addEventListener("click", function () {
-        display.value += operator.textContent
-    })
-})
+        display.value += operator.textContent;
+    });
+});
 
-equal.addEventListener("click", () => {
-    display.value = eval(display.value)
-})
+function calculate() {
+    try {
+        display.value = eval(display.value); // String-i riyazi hesablama üçün eval-a verir
+    } catch {
+        display.value = "Error"; // Səhv daxil edilərsə
+    }
+}
+
+equal.addEventListener("click", calculate);
 
 clear.addEventListener("click", () => {
-    display.value = ""
-})
+    display.value = "";
+});
+
+// Enter düyməsi ilə hesablasın
+document.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        calculate();
+    }
+});
